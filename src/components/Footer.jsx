f0725132b2f395bd904e4bd0a2718c4aef765091
Arsh <iqbalarsh54@gmail.com>
2024-09-29 20:21:56 +0530

    add files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <hr />
+      <footer>
+        <div className=" footer">
+          <p className="">&copy; 2024 All rights reserved.</p>
+          <p>Made with ❤️ by Arsh</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
